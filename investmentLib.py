deb139aa75c8155465e4230481c1c819111afb6c
Jason <GenericCarbonLifeform@gmail.com>
2013-08-07 22:37:16 -0400

    Remove creation of symbol csv files from grabSymbol()

--- investmentLib.py
+++ investmentLib.py
@@ -18,7 +18,6 @@ def grabSymbol(symbol, sDate, eDate):
     urlStr = 'http://ichart.finance.yahoo.com/table.csv?s={0}&a={1}&b={2}&c={3}&d={4}&e={5}&f={6}'.\
     format(symbol.upper(),sDate[1]-1,sDate[2],sDate[0],eDate[1]-1,eDate[2],eDate[0])
     print 'Downloading from ', urlStr
-    urlretrieve(urlStr,symbol+'.csv')
     lines = urlopen(urlStr).readlines()
     
     
